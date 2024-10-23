0fc9860a7f719ba81bfc45f87a45f9201fe68ca1
wasioto <wasioto141@gmail.com>
2024-10-23 18:49:17 -0400

    Updated FizzBuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,15 @@
 # add your code here
-
+#print numbers 1-100
+for i in range(1, 101):
+#if dividable by 3 print Fizz
+    if i%3==0:
+        print("Fizz")
+#if dividable by 5 print Buzz
+    elif i%5==0:
+        print("Buzz")
+#if dividable by 3 & 5 print FizzBuzz
+    elif i%3==0 and i%5==0:
+        print("FizzBuzz")
+#if not dividable by 3 5 or 3 & 5 print number
+else:
+    print(i)
